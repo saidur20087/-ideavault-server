ce99f4a17dced8d7bbc0efcc0b4ecc395a1e4b16
saidur20087 <saidur.20087@gmail.com>
2026-05-20 13:27:25 +0600

    authtication done

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+   origin :["https://ideavault-client-olive.vercel.app"],credentials:true
+}));
 app.use(express.json());
 
 // Mongo URI
